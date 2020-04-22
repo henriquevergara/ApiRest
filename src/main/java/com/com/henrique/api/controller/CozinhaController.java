01a0bc9ea14f90ac8d3e6b8142b3b2c087c97b4f
Henrique <henriquevergara@hotmail.com>
2020-04-22 16:05:49 -0300

    Implementando conent negotiation XML e JSON

--- src/main/java/com/com/henrique/api/controller/CozinhaController.java
+++ src/main/java/com/com/henrique/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.com.henrique.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE }) // atributo utilizado para setar os content negotiation da requisicao, endpoint retorna apenas em xml e json.
 	public List<Cozinha> listar(){
 		return cozinhaRepository.findAll();
 	}
