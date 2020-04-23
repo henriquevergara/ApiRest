78e05d31dc202b0d98f16b996da8822930964fdc
Henrique <henriquevergara@hotmail.com>
2020-04-23 10:57:02 -0300

    Implementando parcialmente o delete

--- src/main/java/com/com/henrique/api/controller/CozinhaController.java
+++ src/main/java/com/com/henrique/api/controller/CozinhaController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -75,6 +77,28 @@ public class CozinhaController {
 
 			return ResponseEntity.notFound().build();
 
+		}
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+
+		try {
+
+			if (cozinhaRepository.findById(id).isPresent()) {
+
+				Cozinha cozinha = cozinhaRepository.findById(id).get();
+				cozinhaRepository.delete(cozinha);
+
+				return ResponseEntity.noContent().build();
+
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+
+		} catch (DataIntegrityViolationException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 
 	}
